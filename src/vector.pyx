90d1f7b72e7eb85b142315583cabb7a3b2c1c7d1
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2014-01-06 10:24:59 +0100

    minor fix on _del function

--- src/vector.pyx
+++ src/vector.pyx
@@ -5,19 +5,18 @@ cdef extern from "nvector.h":
         double x
         double y
 
-    extern nvector* nvector_zero()
     nvector* nvector_new(double x, double y)
     nvector* nvector_copy(nvector *v)
-    extern nvector* nvector_del(nvector *v)
+    void nvector_del(nvector *v)
     void nvector_add(nvector *left, nvector *right)#
-    extern void nvector_sub(nvector *left,  nvector *right)#
-    extern void nvector_mul(nvector *left, double right)#
-    extern double nvector_dot( nvector *left,  nvector *right)#
-    extern double nvector_mag( nvector *v)#
-    extern nvector* nvector_normalize( nvector *v)
-    extern void nvector_setmag(nvector *v, double mag)
-    extern double nvector_angle_between( nvector *v1,  nvector *v2)#
-    extern double nvector_angle( nvector *v)#
+    void nvector_sub(nvector *left,  nvector *right)#
+    void nvector_mul(nvector *left, double right)#
+    double nvector_dot( nvector *left,  nvector *right)#
+    double nvector_mag( nvector *v)#
+    nvector* nvector_normalize( nvector *v)
+    void nvector_setmag(nvector *v, double mag)
+    double nvector_angle_between( nvector *v1,  nvector *v2)#
+    double nvector_angle( nvector *v)#
 
 
 cdef class Vector:
@@ -27,6 +26,10 @@ cdef class Vector:
 
     def __cinit__(self, double x=0.0, double y=0.0):
         self._vector = <nvector*> nvector_new(x,y)
+
+    def __dealloc__(Vector self):
+        nvector_del(<nvector*> self._vector)
+        nvector_del(<nvector*> self._aux_vector)
     
     def __repr__(self):
         return '<Vector({},{})>'.format(self._vector.x, self._vector.y)

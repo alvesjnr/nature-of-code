68488c21c6e6dfdfa6ddd9e03e7950f5926a1e97
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2014-01-06 10:10:29 +0100

    refactoring __mul__ implementing mag, __abs__, angle, angle_between

--- src/vector.pyx
+++ src/vector.pyx
@@ -1,24 +1,23 @@
 
 cdef extern from "nvector.h":
-    
-    # cdef struct nvector:
+
     ctypedef struct nvector:
         double x
         double y
 
-    # extern nvector* nvector_zero()
+    extern nvector* nvector_zero()
     nvector* nvector_new(double x, double y)
     nvector* nvector_copy(nvector *v)
-    # extern nvector* nvector_del(nvector *v)
-    void nvector_add(nvector *left, nvector *right)
-    # extern void nvector_sub(nvector *left,  nvector *right)
-    extern void nvector_mul(nvector *left, double right)
-    extern double nvector_dot( nvector *left,  nvector *right)
-    # extern double nvector_mag( nvector *v)
-    # extern nvector* nvector_normalize( nvector *v)
-    # extern void nvector_setmag(nvector *v, double mag)
-    # extern double nvector_angle_between( nvector *v1,  nvector *v2)
-    # extern double nvector_angle( nvector *v)
+    extern nvector* nvector_del(nvector *v)
+    void nvector_add(nvector *left, nvector *right)#
+    extern void nvector_sub(nvector *left,  nvector *right)#
+    extern void nvector_mul(nvector *left, double right)#
+    extern double nvector_dot( nvector *left,  nvector *right)#
+    extern double nvector_mag( nvector *v)#
+    extern nvector* nvector_normalize( nvector *v)
+    extern void nvector_setmag(nvector *v, double mag)
+    extern double nvector_angle_between( nvector *v1,  nvector *v2)#
+    extern double nvector_angle( nvector *v)#
 
 
 cdef class Vector:
@@ -43,23 +42,53 @@ cdef class Vector:
         else:
             return NotImplemented
 
+    def __sub__(Vector left, Vector right):
+
+        #__sub__ is left - right
+
+        if isinstance(right, Vector) and isinstance(left, Vector):
+            left._aux_vector = <nvector*> nvector_copy(<nvector*>left._vector)
+            nvector_sub( <nvector*> left._aux_vector, <nvector*>right._vector)
+
+            v = Vector(left._aux_vector.x, left._aux_vector.y)
+            return v
+        else:
+            return NotImplemented
+
     def __mul__(left, right):
 
         if isinstance(left, (int, float)) ^ isinstance(right, (int, float)):
             #lets put int/float at left side
             if isinstance(right, (int,float)):
                 right, left = left, right
-            (<Vector>right)._aux_vector = <nvector*> nvector_copy(<nvector*>(<Vector>right)._vector)
-            nvector_mul(<nvector*>(<Vector>right)._aux_vector, left)
-            return Vector((<Vector>right)._aux_vector.x, (<Vector>right)._aux_vector.y)
+            return (<Vector>right).mul(left)
         
         elif isinstance(right, Vector) and isinstance(left, Vector):
-            right = (<Vector>right)
-            left = (<Vector>left)
-            return nvector_dot(<nvector*>(<Vector>right)._vector, <nvector*>(<Vector>left)._vector)
+            return (<Vector>left).dot(right)
 
         else:
             return NotImplemented
 
-    def print_xy(self):
-        print self._vector.x, self._vector.y
+    cdef mul(Vector self, float m):
+        self._aux_vector = <nvector*> nvector_copy(<nvector*>self._vector)
+        nvector_mul(<nvector*>self._aux_vector, m)
+        return Vector(self._aux_vector.x, self._aux_vector.y)
+
+    cdef dot(Vector self, Vector right):
+        return nvector_dot(<nvector*>self._vector, <nvector*>right._vector)
+
+    cdef cross(Vector left, Vector right):
+        return NotImplemented
+
+    cdef mag(Vector self):
+        return nvector_mag(<nvector*> self._vector)
+
+    def __abs__(self):
+        return self.mag()
+
+    cdef angle(Vector self):
+        return nvector_angle(<nvector*> self._vector)
+
+    cdef angle_between(Vector self, Vector other):
+        return nvector_angle_between(<nvector*> self._vector, <nvector*> other._vector)
+
